3f63fe7ae678c7ac67b67414d4afc07105f93faf
Lizeth Daniela Castellanos Alfonso <ld.castellanos@uniandes.edu.co>
2023-03-11 09:23:34 -0500

    Feature/dependencies (#1)
    * :sparkles: Material ui and font
    
    * Login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Outfit } from 'next/font/google'
+
+import '@/styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const outfit = Outfit({ subsets: ['latin'] })
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '',
+  },
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0987A0',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+  }
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+
+    <div className={outfit.className}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+
+  )
 }
