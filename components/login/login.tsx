3e77f35970b8890200a3a2a32bf00ca2190c2fe8
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-03-11 09:20:44 -0500

    Login page

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,19 +1,44 @@
-import { Grid } from "@mui/material";
+import Image from 'next/image'
+import { Button, Grid, Link, Stack, TextField, Typography } from "@mui/material";
+
+import LoginImg from "@/images/login.png"
+import styles from "./login.module.scss"
 
 export default function Login() {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-                <div>xs=8</div>
+        <Grid container spacing={2} className={styles.container}>
+            <Grid item xs={7}>
+                <figure>
+                    <Image
+                        src={LoginImg}
+                        alt="Login"
+
+                    />
+                </figure>
             </Grid>
-            <Grid item xs={4}>
-                <div>xs=4</div>
-            </Grid>
-            <Grid item xs={4}>
-                <div>xs=4</div>
-            </Grid>
-            <Grid item xs={8}>
-                <div>xs=8</div>
+            <Grid item xs={5}>
+                <Stack spacing={2} sx={{ height: "100%" }} padding={4}>
+                    <Stack direction="row" spacing={2} alignItems="center" justifyContent="right">
+                        <div>
+                            ¿Aún no tienes cuenta?
+                        </div>
+                        <Link href="#">Registrarte</Link>
+                    </Stack>
+                    <Stack spacing={2} alignItems="center" justifyContent="center" sx={{ height: "100%" }} maxWidth="300px" alignSelf="center">
+                        <Stack alignItems="center">
+                            <Typography variant="h5" textAlign="center">
+                                Estamos felices de verte nuevamente
+                            </Typography>
+                            <Typography variant="h6" >
+                                Inicia sesión
+                            </Typography>
+                        </Stack>
+
+                        <TextField id="standard-basic" label="Correo" variant="standard" fullWidth />
+                        <TextField id="standard-basic" label="Contraseña" variant="standard" fullWidth />
+                        <Button variant="contained" fullWidth>Ingresar</Button>
+                    </Stack>
+                </Stack>
             </Grid>
         </Grid>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,34 @@ import type { AppProps } from 'next/app'
 import { Outfit } from 'next/font/google'
 
 import '@/styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const outfit = Outfit({ subsets: ['latin'] })
 
 
+const theme = createTheme({
+  typography: {
+    fontFamily: '',
+  },
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0987A0',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+  }
+});
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
+
     <div className={outfit.className}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
+
   )
 }
